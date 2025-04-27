66fc5b5ae9af8d7900f52d27f83862e43a5163cc
Ricardo Jofré Sarria <rjofres@woosoluciones.com>
2025-04-27 19:40:25 -0400

    👽️   Instalacion de yii2 basic

---
+++ app/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql;dbname=caminodeldev',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+    'tablePrefix' => 'cdd_'
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
